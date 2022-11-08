314e91a1eef61d3335cf67e21e30b2bd1f92b719
mattand <mjande@umich.edu>
2022-11-08 09:45:51 -0500

    Add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,17 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
   module: {
     rules: [
       {
